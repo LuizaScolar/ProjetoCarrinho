4e2823946756e55a781e5db9cfc1227aed8980e7
Luiza Scolar <scolar441@gmail.com>
2026-04-28 08:32:49 -0300

    primeiro commit

---
+++ NovoProjeto-PWIII-carrinho_de_compras/Program.cs
@@ -0,0 +1,62 @@
+using NovoProjeto_PWIII_carrinho_de_compras.repository;
+using NovoProjeto_PWIII_carrinho_de_compras.repository.contract;
+using System.Net;
+using NovoProjeto_PWIII_carrinho_de_compras.GerenciaArquivos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddHttpContextAcessor();
+
+//Adicionar a interface como serviço
+builder.Services.AddScoped<ILivroRepository, LivroRepository>();
+builder.Services.AddScoped<IEmprestimoRepository, EmprestimoRepository>();
+builder.Services.AddScoped<IItemRepository, ItemRepository>();
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context +> true;
+    options.MinimumSameSitePolicy = SameSiteMode.none;
+});
+
+// Corrigir problema com TEMPDATA para aumentar o tempo de duração
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    // Set a short timeout for easy testing.
+    options.IdleTimeout = TimeSpan.FromSeconds(900);
+    options.Cookie.HttpOnly = true;
+    // Deixar informado para o navegador que a sessão é essencial
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddMvc().AddSessionStateTempDataProvider();
+
+builder.Services.AddMemoryCache(); // Guardar os dados na memoria
+
+//Add Gerenciador Arquivo como serviços
+builder.Services.AddScoped<GerenciadorArquivo>();
+builder.Services.AddScoped<EmprestimoLiivroAspCore.Cookie.Cookie>();
+builder.Services.AddScoped<EmprestimoLiivroAspCore.CarrinhoCompra.CookieCarrinhoCompra>();
+
+var app = builder.Build();
+
+builder.Services.AddScoped<ILivroRepository, LivroRepository>();
+builder.Services.AddScoped<IItemRepository, ItemRepository>();
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
